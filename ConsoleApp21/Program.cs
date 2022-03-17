3d819bb54d3a7f3c568763a56a929e369b0bca3a
bogor <bogor@VELP>
2022-03-17 23:35:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+                Console.Write("Введите кол-во элементов массива: ");
+
+                int n = int.Parse(Console.ReadLine());
+
+                int[] Arr = new int[n];
+
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    Console.Write($"\nВведите элемент массива под индексом {i}: ");
+                    Arr[i] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"\nВывод массива:  ");
+
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                Console.Write(Arr[i] + " ");
+                }
+
+               
+            
+        
+
+        int a, b;
+            Console.Write("\n\nВведите целое число a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите целое число b = ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nЭлементы из интервала [{a}, {b}]: \n");
+
+            int count = Task3.Pick(Arr, a, b);
+
+
+
+
+            Task3.Check(count);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp21/Task3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Task3
+    {
+        public static void Task3All(int[] Arr, int a, int b, int count)
+        {
+            PrintMass(Arr);
+            Pick(Arr, a, b);
+            Check(count);
+        }
+
+       
+
+            public static void PrintMass(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+                Console.Write(Arr[i] + " ");
+        }
+
+
+        public static int Pick(int[] Arr, int a, int b)
+        {
+
+
+            int count = 0;
+
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                if ((Arr[i] >= a) && (Arr[i] <= b))
+                {
+                    Console.Write(Arr[i] + " ");
+                    count++;
+                }
+            }
+            return count;
+        }
+
+       public static void Check(int count)
+        {
+
+            
+            if (count != 0)
+                Console.WriteLine($"\nКол-во элементов: {count} ");
+            else
+                Console.WriteLine("Элементов, которые входят в интервал a, b нет!");
+            
+        }
+    }
+}
